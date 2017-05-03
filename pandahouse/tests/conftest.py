cab141989fe39d49835c08e1b2cc5e84a0f6c4bb
Szucs Krisztian <szucs.krisztian@gmail.com>
2017-05-03 12:44:23 +0200

    correct test host

--- pandahouse/tests/conftest.py
+++ pandahouse/tests/conftest.py
@@ -1,10 +1,6 @@
 import pytest
 
-import numpy as np
-import pandas as pd
-
 
 @pytest.fixture(scope='session')
 def host():
-    return 'http://bdas-worker-4:8123'
     return 'https://localhost:8123'
